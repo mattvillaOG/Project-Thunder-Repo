153de0e8b62c4a1aebc313db277f90a03bdcc56d
mattvillaOG <113946048+mattvillaOG@users.noreply.github.com>
2026-01-16 17:57:31 -0500

    Updated Menus
    I hooked up instructions and credits to the main menu navigation system

--- MobileRacer/Assets/Scripts/LoadGame.cs
+++ MobileRacer/Assets/Scripts/LoadGame.cs
@@ -5,9 +5,42 @@ using UnityEngine.SceneManagement;
 
 public class LoadGame : MonoBehaviour
 {
+    public GameObject instructions;
+    public GameObject credits;
+
+
+    private void Start()
+    {
+        //disable these menus on start
+        instructions.SetActive(false);
+        credits.SetActive(false);
+    }
+
     //this is the most exciting script ever
     public void BeginGame()
     {
         SceneManager.LoadScene("MainLevel");
+    }
+
+    //used to navgate instrucions
+    public void OpenInstructions()
+    {
+        instructions.SetActive(true);
+    }
+
+    public void CloseInstructions()
+    {
+        instructions.SetActive(false);
+    }
+
+    //used to navgate creadits
+    public void OpenCredits()
+    {
+        credits.SetActive(true);
+    }
+
+    public void CloseCredits()
+    {
+        credits.SetActive(false);
     }
 }
